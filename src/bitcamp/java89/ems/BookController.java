fa725ad1067a703b4c5ba7a3b65f84ff1ecfede8
GraysonSunkyoJung <graysons.jung@gmail.com>
2016-10-28 11:27:36 +0900

    .

--- src/bitcamp/java89/ems/BookController.java
+++ src/bitcamp/java89/ems/BookController.java
@@ -3,28 +3,33 @@ package bitcamp.java89.ems;
 import java.util.Scanner;
 
 public class BookController {
-  static Book[] books = new Book[100];
-  static int length = 0;
-  static Scanner keyScan;
+  Book[] books = new Book[100];
+  int length = 0;
+  Scanner keyScan;
 
-  static void doAdd() {
-    while (length < books.length){
+  public BookController(Scanner keyScan) {
+    this.keyScan = keyScan;
+  }
+
+  void doAdd() {
+    while (length < this.books.length){
       Book b1 = new Book();
       System.out.print("제목? ");
-      b1.name = keyScan.nextLine();
+      b1.name = this.keyScan.nextLine();
 
       System.out.print("저자? ");
-      b1.author = keyScan.nextLine();
+      b1.author = this.keyScan.nextLine();
 
       System.out.print("가격? ");
-      b1.price = Integer.parseInt(keyScan.nextLine());
+      b1.price = Integer.parseInt(this.keyScan.nextLine());
 
       System.out.print("쪽수? ");
-      b1.page = Integer.parseInt(keyScan.nextLine());
+      b1.page = Integer.parseInt(this.keyScan.nextLine());
 
       System.out.print("부록(y/n)? ");
-      b1.cd = keyScan.nextLine().equals("y") ? true : false;
-      books[length++] = b1;
+      b1.cd = this.keyScan.nextLine().equals("y") ? true : false;
+
+      this.books[length++] = b1;
 
       System.out.print("계속 입력하시겠습니까(y/n)?");
       if (!keyScan.nextLine().equals("y"))
@@ -32,23 +37,23 @@ public class BookController {
 
     }
   }
-  static void doView() {
+  void doView() {
     System.out.print("조회할 책의 제목은?");
-    String name = keyScan.nextLine().toLowerCase();
-    for (int i = 0; i < length; i++) {
-      if(books[i].name.toLowerCase().equals(name)){
-        System.out.printf("제목: %s\n", books[i].name);
-        System.out.printf("저자: %s\n", books[i].author);
-        System.out.printf("가격: %d\n", books[i].price);
-        System.out.printf("쪽수: %d\n", books[i].page);
-        System.out.printf("부록: %b\n", books[i].cd);
+    String name = this.keyScan.nextLine().toLowerCase();
+    for (int i = 0; i < this.length; i++) {
+      if(this.books[i].name.toLowerCase().equals(name)){
+        System.out.printf("제목: %s\n", this.books[i].name);
+        System.out.printf("저자: %s\n", this.books[i].author);
+        System.out.printf("가격: %d\n", this.books[i].price);
+        System.out.printf("쪽수: %d\n", this.books[i].page);
+        System.out.printf("부록: %b\n", this.books[i].cd);
         break;
       }
     }
   }
-  static void doList() {
-    for (int i = 0; i <length; i++) {
-      Book b1 = books[i];
+  void doList() {
+    for (int i = 0; i <this.length; i++) {
+      Book b1 = this.books[i];
       System.out.printf("%s, %s, %d, %d, %b\n",
         b1.name,
         b1.author,
--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -6,7 +6,7 @@ public class EduApp {
 
   public static void main(String[] args) {
     //EduApp에서 사용하는 keyScan을 BookController와 공유한다.
-    BookController.keyScan = keyScan;
+    BookController bookController = new BookController(keyScan);
 
     System.out.println("비트캠프 관리 시스템에 오신걸 환영합니다.");
 
@@ -15,9 +15,9 @@ public class EduApp {
         System.out.print("명령> ");
         String command = keyScan.nextLine().toLowerCase(); //입력받은걸 그대로 출력
         switch (command) {
-        case "add": BookController.doAdd(); break;
-        case "list": BookController.doList(); break;
-        case "view": BookController.doView(); break;
+        case "add": bookController.doAdd(); break;
+        case "list": bookController.doList(); break;
+        case "view": bookController.doView(); break;
         case "quit":
             System.out.println("Good bye!");
               break loop;
@@ -25,5 +25,54 @@ public class EduApp {
               System.out.println("지원하지 않는 명령어 입니다.");
         }
       }
+
     }
   }
+
+
+/*
+    //여러명의 학생 정보를 입력하기 위한 레퍼런스 배열을 만든다.
+    Book[] books = new Book[100];
+    int length = 0; //레퍼런스 배열에 몇명의 학생 정보를 저장했는지 개수를 보관한다.
+                    //레퍼런스 배열에 몇개의 Book 인스턴스가 들어있는지 그 개수를 보관한다.
+
+    Scanner keyScan = new Scanner(System.in);
+
+    //반복해서 입력 받는다.
+    //Book b1 = null; //null 주소없음을 의미
+    while (length < books.length){
+      Book b1 = new Book();
+      System.out.print("제목? ");
+      b1.name = keyScan.nextLine();
+
+      System.out.print("저자? ");
+      b1.author = keyScan.nextLine();
+
+      System.out.print("가격? ");
+      b1.price = Integer.parseInt(keyScan.nextLine());
+
+      System.out.print("쪽수? ");
+      b1.page = Integer.parseInt(keyScan.nextLine());
+
+      System.out.print("부록(y/n)? ");
+      b1.cd = keyScan.nextLine().equals("y") ? true : false;
+      books[length++] = b1;
+
+      System.out.print("계속 입력하시겠습니까(y/n)?");
+      if (!keyScan.nextLine().equals("y"))
+        break;
+    }
+    printStudentList(books, length);
+  }
+
+  static void printStudentList(Book[] books, int length) {
+    //Book b1 = null;
+    for (int i = 0; i <length; i++){
+      Book b1 = books[i];
+      System.out.printf("%s, %s, %d, %d, %b\n",
+        b1.name,
+        b1.author,
+        b1.price,
+        b1.page,
+        ((b1.cd)? "y" : "n"));
+    }*/
