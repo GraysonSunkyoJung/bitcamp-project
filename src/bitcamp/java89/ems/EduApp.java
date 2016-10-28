1cbc93cfeeb48aa6d16d8d4793009cfe8bbc112a
GraysonSunkyoJung <graysons.jung@gmail.com>
2016-10-28 11:18:51 +0900

    .

--- src/bitcamp/java89/ems/EduApp.java
+++ src/bitcamp/java89/ems/EduApp.java
@@ -1,69 +1,29 @@
 package bitcamp.java89.ems;
-
 import java.util.Scanner;
-
 public class EduApp {
+
+  static Scanner keyScan = new Scanner(System.in);
+
   public static void main(String[] args) {
+    //EduApp에서 사용하는 keyScan을 BookController와 공유한다.
+    BookController.keyScan = keyScan;
+
     System.out.println("비트캠프 관리 시스템에 오신걸 환영합니다.");
 
-  Book[] books = new Book[100];
-  int length = 0;
-  Scanner keyScan = new Scanner(System.in);
-
-    loop:
-    while (true){
-      System.out.print("명령> ");
-      String command = keyScan.nextLine().toLowerCase(); //입력받은걸 그대로 출력
-    switch (command) {
-    case "add":
-      while (length < books.length){
-        Book b1 = new Book();
-        System.out.print("제목? ");
-        b1.name = keyScan.nextLine();
-
-        System.out.print("저자? ");
-        b1.author = keyScan.nextLine();
-
-        System.out.print("가격? ");
-        b1.price = Integer.parseInt(keyScan.nextLine());
-
-        System.out.print("쪽수? ");
-        b1.page = Integer.parseInt(keyScan.nextLine());
-
-        System.out.print("부록(y/n)? ");
-        b1.cd = keyScan.nextLine().equals("y") ? true : false;
-        books[length++] = b1;
-
-        System.out.print("계속 입력하시겠습니까(y/n)? ");
-        if (!keyScan.nextLine().equals("y"))
-          break;
-      }
-      break;
-      case "list":
-        printStudentList(books, length);
-        break;
-      case "view":
-        System.out.println(command);
-        break;
-      case "quit":
-        System.out.println("Good bye!");
-        break loop;
-      default:
-          System.out.println("지원하지 않는 명령어 입니다.");
+      loop:
+      while (true) {
+        System.out.print("명령> ");
+        String command = keyScan.nextLine().toLowerCase(); //입력받은걸 그대로 출력
+        switch (command) {
+        case "add": BookController.doAdd(); break;
+        case "list": BookController.doList(); break;
+        case "view": BookController.doView(); break;
+        case "quit":
+            System.out.println("Good bye!");
+              break loop;
+            default:
+              System.out.println("지원하지 않는 명령어 입니다.");
+        }
       }
     }
   }
-
-  static void printStudentList(Book[] books, int length) {
-    //Book b1 = null;
-    for (int i = 0; i <length; i++){
-      Book b1 = books[i];
-      System.out.printf("%s, %s, %d, %d, %b\n",
-        b1.name,
-        b1.author,
-        b1.price,
-        b1.page,
-        ((b1.cd)? "y" : "n"));
-    }
-  }
-}
